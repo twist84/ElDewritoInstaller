cefc9dc9d10e9ecbb7b970ab950f929b66baf158
theTwist84 <lee.foulger.94@gmail.com>
2016-02-13 22:09:46 +0000

    Initial code

---
+++ ElDewritoInstaller/MainWindow.xaml.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+using Ionic.Zip;
+using Newtonsoft.Json;
+
+namespace ElDewritoInstaller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region BackgroundWorkers
+        private static BackgroundWorker Download;
+        private static BackgroundWorker HashCheck;
+        private static BackgroundWorker Extract;
+        #endregion
+
+        #region Variables
+        WebClient webClient = new WebClient();
+        string dewLoc = Path.Combine(Directory.GetCurrentDirectory(), JSON("name"));
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), JSON("filename"));
+        string fileHash = JSON("hash");
+        string url = JSON("url");
+        #endregion
+
+        #region Main
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (File.Exists(filePath))
+            { this.btnStartDownload.IsEnabled = false; this.btnStartHashCheck.IsEnabled = true; this.btnStartInstall.IsEnabled = false; }
+            else
+            { this.btnStartDownload.IsEnabled = true; this.btnStartHashCheck.IsEnabled = false; this.btnStartInstall.IsEnabled = false; }
+        }
+        #endregion
+
+        #region Button classes
+        private void btnStartDownload_Click(object sender, EventArgs e)
+        {
+            Download = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Download_RunWorkerCompleted);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(Download_ProgressChanged);
+            webClient.DownloadFileAsync(new Uri(url), filePath);
+
+            this.btnStartDownload.IsEnabled = false;
+            this.btnStartHashCheck.IsEnabled = false;
+            this.btnStartInstall.IsEnabled = false;
+        }
+
+        private void btnStartHashCheck_Click(object sender, EventArgs e)
+        {
+            HashCheck = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+
+            HashCheck.DoWork += HashCheck_DoWork;
+            HashCheck.ProgressChanged += HashCheck_ProgressChanged;
+            HashCheck.RunWorkerCompleted += HashCheck_RunWorkerCompleted;
+
+            HashCheck.RunWorkerAsync(filePath);
+            this.btnStartDownload.IsEnabled = false;
+            this.btnStartHashCheck.IsEnabled = false;
+            this.btnStartInstall.IsEnabled = false;
+        }
+
+        private void btnStartInstall_Click(object sender, RoutedEventArgs e)
+        {
+            Extract = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+
+            Extract.DoWork += Extract_DoWork;
+            Extract.ProgressChanged += Extract_ProgressChanged;
+            Extract.RunWorkerCompleted += Extract_RunWorkerCompleted;
+
+            Extract.RunWorkerAsync(filePath);
+            this.btnStartDownload.IsEnabled = false;
+            this.btnStartHashCheck.IsEnabled = false;
+            this.btnStartInstall.IsEnabled = false;
+        }
+        #endregion
+
+        #region Download classes
+        private void Download_ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            string progressPercentage = Convert.ToString(e.ProgressPercentage);
+            string receivedMB = ConvertBytesToMegaGigabytes(e.BytesReceived, "MB").ToString("0.00");
+            string receivedGB = ConvertBytesToMegaGigabytes(e.BytesReceived, "GB").ToString("0.00");
+            string receiveMB = ConvertBytesToMegaGigabytes(e.TotalBytesToReceive, "MB").ToString("0");
+            string receiveGB = ConvertBytesToMegaGigabytes(e.TotalBytesToReceive, "GB").ToString("0.00");
+
+            progressBar.Value = e.ProgressPercentage;
+            if (ConvertBytesToMegaGigabytes(e.BytesReceived, "MB") < 1024)
+            {
+                progressBarText.Text = String.Format("{0}% {1}/{2}MB", progressPercentage, receivedMB, receiveMB);
+            }
+            else
+            {
+                progressBarText.Text = String.Format("{0}% {1}/{2}GB", progressPercentage, receivedGB, receiveGB);
+            }
+        }
+
+        private void Download_RunWorkerCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download completed!");
+            progressBar.Value = 0;
+            this.btnStartHashCheck.IsEnabled = true;
+        }
+        #endregion
+
+        #region HashCheck classes
+        private void HashCheck_DoWork(object sender, DoWorkEventArgs e)
+        {
+            byte[] buffer;
+            int bytesRead;
+            long size;
+            long totalBytesRead = 0;
+
+            using (Stream file = File.OpenRead(filePath))
+            {
+                size = file.Length;
+
+                using (HashAlgorithm hasher = SHA256.Create())
+                {
+                    do
+                    {
+                        buffer = new byte[4096];
+
+                        bytesRead = file.Read(buffer, 0, buffer.Length);
+
+                        totalBytesRead += bytesRead;
+
+                        hasher.TransformBlock(buffer, 0, bytesRead, null, 0);
+
+                        HashCheck.ReportProgress((int)((double)totalBytesRead / size * 100));
+
+                    }
+                    while (bytesRead != 0);
+
+                    hasher.TransformFinalBlock(buffer, 0, 0);
+
+                    e.Result = MakeHashString(hasher.Hash);
+                }
+            }
+        }
+
+        private void HashCheck_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string progressPercentage = Convert.ToString(e.ProgressPercentage);
+
+            progressBar.Value = e.ProgressPercentage;
+            progressBarText.Text = String.Format("{0}%", progressPercentage);
+        }
+
+        private void HashCheck_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Result.ToString() == fileHash)
+            {
+                MessageBox.Show("Hash Check Completed Successfully!");
+                progressBar.Value = 0;
+                this.btnStartHashCheck.IsEnabled = false;
+                this.btnStartInstall.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Hash Check Completed Unsuccessfully, Please Redownload!");
+                this.btnStartDownload.IsEnabled = true;
+                this.btnStartHashCheck.IsEnabled = false;
+            }
+        }
+        #endregion
+
+        #region Extract classes
+        private void Extract_DoWork(object sender, DoWorkEventArgs e)
+        {
+            using (ZipFile zip = ZipFile.Read(filePath))
+            {
+                zip.ExtractProgress +=
+                    new EventHandler<ExtractProgressEventArgs>(zip_ExtractProgress);
+                zip.ExtractAll(dewLoc, ExtractExistingFileAction.OverwriteSilently);
+            }
+        }
+
+        private void Extract_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string progressPercentage = Convert.ToString(e.ProgressPercentage);
+
+            progressBar.Value = e.ProgressPercentage;
+            progressBarText.Text = String.Format("{0}%", progressPercentage);
+        }
+
+        private void Extract_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Install Completed Successfully!");
+            progressBar.Value = 0;
+            this.btnStartDownload.IsEnabled = true;
+            this.btnStartHashCheck.IsEnabled = true;
+            this.btnStartInstall.IsEnabled = true;
+        }
+        #endregion
+
+        #region Helper classes
+        static string JSON(string Option)
+        {
+            System.Net.WebClient wc = new System.Net.WebClient();
+            dynamic dew = JsonConvert.DeserializeObject(wc.DownloadString("http://thetwist84.github.io/HaloOnlineModManager/game/base.json"));
+
+            string name = dew["base"].Name;
+            string version = dew["base"].Version;
+            string filename = dew["base"].Filename;
+            string hash = dew["base"].Hash;
+            string url = dew["base"].Url;
+
+            if (Option == "name")
+                return name;
+            if (Option == "version")
+                return version;
+            if (Option == "filename")
+                return filename;
+            if (Option == "hash")
+                return hash;
+            if (Option == "url")
+                return url;
+            return null;
+        }
+
+        static double ConvertBytesToMegaGigabytes(long bytes, string Option)
+        {
+            if (Option == "MB")
+                return (bytes / 1024f) / 1024f;
+            if (Option == "GB")
+                return ((bytes / 1024f) / 1024f) / 1024.0;
+            return 0;
+        }
+
+        private static string MakeHashString(byte[] hashBytes)
+        {
+            StringBuilder hash = new StringBuilder(64);
+
+            foreach (byte b in hashBytes)
+                hash.Append(b.ToString("X2").ToUpper());
+
+            return hash.ToString();
+        }
+
+        void zip_ExtractProgress(object sender, ExtractProgressEventArgs e)
+        {
+            if (e.TotalBytesToTransfer > 0)
+            {
+                Extract.ReportProgress(Convert.ToInt32(100 * e.BytesTransferred / e.TotalBytesToTransfer));
+            }
+        }
+        #endregion
+    }
+}
